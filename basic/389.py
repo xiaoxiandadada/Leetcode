459adfe66c5767d9e60c179130ebe6d6a3aa2547
Fairy Yang <yangchen5@stu.scu.edu.cn>
2025-11-23 23:52:14 +0800

    add 389.py

---
+++ basic/389.py
@@ -0,0 +1,14 @@
+## 找不同
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        x = 0
+        for ch in s:
+            x ^= ord(ch)
+        for ch in t:
+            x ^= ord(ch)
+        return chr(x)
+
+s = "abcd"
+t = "abcde"
+Sol=Solution()
+print(Sol.findTheDifference(s,t))

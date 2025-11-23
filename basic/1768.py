c41d1d3cba7cb99a307efbe8cf86da7e14b9297c
Fairy Yang <yangchen5@stu.scu.edu.cn>
2025-11-23 22:00:20 +0800

    update 1768.py

--- basic/1768.py
+++ basic/1768.py
@@ -1,23 +1,34 @@
 # 交替合并字符串
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        merge=[]
-        j=max(len(word2),len(word1))
-        i=min(len(word2),len(word1))
+        # merge=[]
+        # j=max(len(word2),len(word1))
+        # i=min(len(word2),len(word1))
         
-        if len(word1) <= len(word2):
-            for i in range(len(word1)):
-                merge.append(word1[i])
-                merge.append(word2[i])
-            merge.append(word2[i+1:j])
+        # if len(word1) <= len(word2):
+        #     for i in range(len(word1)):
+        #         merge.append(word1[i])
+        #         merge.append(word2[i])
+        #     merge.append(word2[i+1:j])
         
-        if len(word1) > len(word2):
-            for i in range(len(word2)):
-                merge.append(word1[i])
-                merge.append(word2[i])
-            merge.append(word1[i+1:j])
+        # if len(word1) > len(word2):
+        #     for i in range(len(word2)):
+        #         merge.append(word1[i])
+        #         merge.append(word2[i])
+        #     merge.append(word1[i+1:j])
         
-        return "".join(merge)
+        # return "".join(merge)
+        res=[]
+        i=j=0
+        while i < len(word1) or j < len(word2):
+            if i < len(word1):
+                res.append(word1[i])
+                i += 1
+            if j < len(word2):
+                res.append(word2[j])
+                j += 1
+        return "".join(res)
+
 
 sol=Solution()
 for word1,word2 in [("abc","pqr"),("ab","pqrs"),("abcd","pq")]:

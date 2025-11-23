5a11e3042953d3ff7f8313dce8d5a9002e8e90b7
Fairy Yang <yangchen5@stu.scu.edu.cn>
2025-11-23 22:14:26 +0800

    update 1768.py(zip)

--- basic/1768.py
+++ basic/1768.py
@@ -18,18 +18,26 @@ class Solution:
         #     merge.append(word1[i+1:j])
         
         # return "".join(merge)
+
+
+        # res=[]
+        # i=j=0
+        # while i < len(word1) or j < len(word2):
+        #     if i < len(word1):
+        #         res.append(word1[i])
+        #         i += 1
+        #     if j < len(word2):
+        #         res.append(word2[j])
+        #         j += 1
+        # return "".join(res)
+
         res=[]
-        i=j=0
-        while i < len(word1) or j < len(word2):
-            if i < len(word1):
-                res.append(word1[i])
-                i += 1
-            if j < len(word2):
-                res.append(word2[j])
-                j += 1
-        return "".join(res)
-
-
+        for a,b in zip(word1,word2):
+            res +=[a,b]
+        return "".join(res)+word1[len(word2):]+word2[len(word1):]
+        
+        
+        
 sol=Solution()
 for word1,word2 in [("abc","pqr"),("ab","pqrs"),("abcd","pq")]:
     print(sol.mergeAlternately(word1,word2))

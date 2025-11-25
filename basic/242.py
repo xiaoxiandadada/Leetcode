7a539a8052121fce1ecb7bced1af020dc22965bc
Fairy Yang <yangchen5@stu.scu.edu.cn>
2025-11-25 21:26:38 +0800

    update 242.py (dic)

--- basic/242.py
+++ basic/242.py
@@ -2,7 +2,24 @@
 from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return len(s) == len(t) and Counter(s) == Counter(t)
+        # return len(s) == len(t) and Counter(s) == Counter(t)
+
+        c1 = {}
+        c2 = {}
+        
+        for c in s:
+            # if c in c1:
+            #     c1[c] += 1
+            # else:
+            #     c1[c] = 1
+            c1[c] = c1.setdefault(c,0) + 1
+        for c in t:
+            c2[c] = c2.setdefault(c,0) + 1
+        
+        return c1==c2
+        
+
+
 
 s = "anagram"
 t = "nagaram"

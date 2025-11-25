616a3cf06cf6e67cc5ef4f6d2208b4207cab2359
Fairy Yang <yangchen5@stu.scu.edu.cn>
2025-11-26 00:14:05 +0800

    add 1015.py and achive maths

---
+++ maths/1015.py
@@ -0,0 +1,14 @@
+class Solution:
+    def smallestRepunitDivByK(self, k: int) -> int:
+        if k % 2 == 0 or k % 5 == 0:
+            return -1
+        length = 1
+        num = 1
+        while num % k != 0:
+            num = (num % k) * 10 + 1
+            length += 1
+        return length
+
+k = 3
+Sol = Solution()
+print(Sol.smallestRepunitDivByK(k))

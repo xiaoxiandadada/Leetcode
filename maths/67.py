f322830b9b833bdef00fb725343cb03bde916110
Fairy Yang <yangchen5@stu.scu.edu.cn>
2025-12-11 22:01:28 +0800

    add 67.py(bin)

---
+++ maths/67.py
@@ -0,0 +1,7 @@
+# 二进制求和
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        return bin(int(a, 2) + int(b, 2))[2:]
+
+print(Solution().addBinary("11", "1"))     # 100
+print(Solution().addBinary("1010", "1011")) # 10101
